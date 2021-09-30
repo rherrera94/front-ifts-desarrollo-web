c676e900bf9f0c2838edefbfa00fbec94a9141ff
rherrera94 <rafael.herrera@outlook.com.ar>
2021-09-30 11:44:33 -0300

    avance final frontend

--- src/componentes/cumpleanios.jsx
+++ src/componentes/cumpleanios.jsx
@@ -12,26 +12,28 @@ export default function Cumpleanios() {
             <div className="cumple-tabla">
                 <h2> Festejemos junto a nuestros compañeros este día tan especial</h2><br/>
                 <table className="tabla-cumpleanios">
-                        <tr> 
-                            <th>Día</th> 
-                            <th>Empleado</th> 
-                            <th>Interno</th>
-                        </tr>
-                        {empleados.map((unEmpleado, index) => {
-                            let d=new Date(unEmpleado.cumpleanios);
-                            let dcomp=new Date();
-                            if(d.getMonth()===dcomp.getMonth()){  
-                                let dia=d.getDate();
-                                return (
-                                    <tr>
-                                        <td>{dia}</td> 
-                                        <td>{unEmpleado.nombre}</td>
-                                        <td>{unEmpleado.interno}</td>
-                                    </tr>         
-                                    );
-                            }
-                            return null;
-                        })}
+                        <tbody>
+                            <tr> 
+                                <th>Día</th> 
+                                <th>Empleado</th> 
+                                <th>Interno</th>
+                            </tr>
+                            {empleados.map((unEmpleado, index) => {
+                                let d=new Date(unEmpleado.cumpleanios);
+                                let dcomp=new Date();
+                                if(d.getMonth()===dcomp.getMonth()){  
+                                    let dia=d.getDate();
+                                    return (
+                                        <tr key={index}>
+                                            <td>{dia}</td> 
+                                            <td>{unEmpleado.nombre}</td>
+                                            <td>{unEmpleado.interno}</td>
+                                        </tr>         
+                                        );
+                                }
+                                return null;
+                            })}
+                        </tbody>
                 </table>
             </div>
         )    
--- src/componentes/login.jsx
+++ src/componentes/login.jsx
@@ -2,22 +2,22 @@
 export default function Login() {
 
     return (
-        <div class="container-login">
-        <div id="login-row" class="row justify-content-center align-items-center">
+        <div className="container-login">
+        <div id="login-row" className="row justify-content-center align-items-center">
             <div id="login-column">
-                <div class="box">
-                    <div class="float">
-                        <form class="form" action="">
-                            <div class="form-group">
-                                <label for="username" class="text-white">Usuario:</label><br/>
-                                <input type="text" name="username" id="username" class="form-control"/>
+                <div className="box">
+                    <div className="float">
+                        <form className="form" action="">
+                            <div className="form-group">
+                                <label className="text-white">Usuario:</label><br/>
+                                <input type="text" name="username" id="username" className="form-control"/>
                             </div>
-                            <div class="form-group">
-                                <label for="password" class="text-white">Contraseña:</label><br/>
-                                <input type="text" name="password" id="password" class="form-control"/>
+                            <div className="form-group">
+                                <label className="text-white">Contraseña:</label><br/>
+                                <input type="text" name="password" id="password" className="form-control"/>
                             </div>
-                            <div class="form-group boton-login">
-                                <input type="submit" name="submit" class="btn btn-info btn-md" value="Ingresar"/>
+                            <div className="form-group boton-login">
+                                <input type="submit" name="submit" className="btn btn-info btn-md" value="Ingresar"/>
                             </div>
                         </form>
                     </div>
